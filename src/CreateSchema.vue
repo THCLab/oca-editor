7203b983b799b1e72ed98a85b9c459fb639b28cf
Marcin <marcin@opensoftware.pl>
2019-11-26 11:59:07 +0100

    Split CreateSchema view to createForm and uploadForm columns

--- src/CreateSchema.vue
+++ src/CreateSchema.vue
@@ -1,65 +1,90 @@
 <template>
   <b-container id="app" class="bv-example-row">
-    <b-row align-h="center">
-      <b-col cols="12">
-        <h1>Schema Base & Overlays</h1>
+    <b-row>
+      <b-col cols="6" class="newFormCol">
+        <b-row align-h="center">
+          <b-col cols="12">
+            <h1>Create Form</h1>
+          </b-col>
+
+          <b-col sm="10" lg="81">
+            <b-form @submit.prevent>
+              <b-form-group
+                id="nameGroup"
+                description="Name of the schema base which you are creating. E.g. Driving licence"
+              >
+                <b-form-input
+                  id="nameInput"
+                  type="text"
+                  v-model="form.name"
+                  required
+                  size="lg"
+                  placeholder="Enter name"
+                />
+              </b-form-group>
+
+              <b-form-group
+                id="descGroup"
+                description="Provide description for schema. E.g. Official driving licence schema issued by Gov"
+              >
+                <b-form-input
+                  id="descInput"
+                  type="text"
+                  v-model="form.description"
+                  required
+                  size="lg"
+                  description="Provide meaningful description for your schama base"
+                  placeholder="Enter description"
+                />
+              </b-form-group>
+            </b-form>
+          </b-col>
+
+          <b-col sm="10" lg="81" class="createBtn">
+            <b-row>
+              <b-col></b-col>
+              <b-col>
+                <b-button block
+                  @click="onCreateForm"
+                  :disabled = "form.name.length == 0"
+                  variant="primary">
+                  Create
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
       </b-col>
 
-      <b-col sm="10" lg="81">
-          <b-form-file
-            v-model="file"
-            id="chooseFile"
-            accept=".zip"
-            :disabled="form.name.length != 0 || form.description.length != 0"
-          />
-      </b-col>
+      <b-col cols="6" class="fileCol">
+        <b-row align-h="center">
+          <b-col cols="12">
+            <h1>Upload Form file</h1>
+          </b-col>
 
-      <b-col sm="10" lg="81">
-        <br>
-      </b-col>
+          <b-col sm="10" lg="81" class="fileInput">
+              <b-form-file
+                v-model="file"
+                size="lg"
+                id="chooseFile"
+                accept=".zip"
+              />
+          </b-col>
 
-      <b-col sm="10" lg="81">
-        <b-form @submit.prevent>
-          <b-form-group
-            id="nameGroup"
-            description="Name of the schema base which you are creating. E.g. Driving licence"
-          >
-            <b-form-input
-              id="nameInput"
-              type="text"
-              v-model="form.name"
-              required
-              size="lg"
-              placeholder="Enter name"
-              :disabled="Boolean(file)"
-            />
-          </b-form-group>
-
-          <b-form-group
-            id="descGroup"
-            description="Provide description for schema. E.g. Official driving licence schema issued by Gov"
-          >
-            <b-form-input
-              id="descInput"
-              type="text"
-              v-model="form.description"
-              required
-              size="lg"
-              description="Provide meaningful description for your schama base"
-              placeholder="Enter description"
-              :disabled="Boolean(file)"
-            />
-          </b-form-group>
-
-          <b-row>
-            <b-col>
-              <b-button block @click="onCreateSchema" variant="primary">Create</b-button>
-            </b-col>
-            <b-col>
-              <b-button block type="reset" @click="resetFileInput" variant="danger">Reset</b-button>
-            </b-col>
-          </b-row>
-        </b-form>
+          <b-col sm="10" lg="81" class="uploadBtn">
+            <b-row>
+              <b-col></b-col>
+              <b-col>
+                <b-button block
+                  @click="onUploadForm"
+                  :disabled = "!Boolean(file)"
+                  variant="primary">
+                  Upload
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-col>
+        </b-row>
       </b-col>
     </b-row>
   </b-container>
@@ -85,24 +110,22 @@ export default {
     };
   },
   methods: {
-    onCreateSchema() {
+    onCreateForm() {
+      // key 'schemas' store schema list, so it can't be overriden
+      if (this.form.name != 'schemas') {
+        const created = save_schema(this.form);
+        if (created) {
+          this.$router.push("schemas");
+        }
+      }
+    },
+    onUploadForm() {
       if (this.file) {
         resolveZipFile(this.file).then(results => {
           results.forEach(schemaData => renderForm(schemaData))
           this.$router.push("schemas");
         })
-      } else {
-        // key 'schemas' store schema list, so it can't be overriden
-        if (this.form.name != 'schemas') {
-          const created = save_schema(this.form);
-          if (created) {
-            this.$router.push("schemas");
-          }
-        }
       }
-    },
-    resetFileInput() {
-      this.file = null
     }
   },
   created() {
@@ -114,5 +137,26 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+  h1 {
+    padding-bottom: 30px;
+  }
+
+  .newFormCol, .fileCol {
+    position: relative;
+    min-height: 360px;
+  }
+
+  .createBtn, .uploadBtn {
+    position: absolute;
+    bottom: 10%;
+  }
+
+  .newFormCol {
+    border-right: 1px solid #ced4da;
+  }
+
+  .fileCol {
+    border-left: 1px solid #ced4da;
+  }
 </style>
