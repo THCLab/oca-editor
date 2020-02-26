69280f7b9bcc74118d95603123e6d1f1b675fafd
Marcin <marcin@opensoftware.pl>
2020-02-26 15:03:27 +0100

    Handle PUBLISH_FORM event

--- src/odcaCommunication.js
+++ src/odcaCommunication.js
@@ -3,6 +3,7 @@ import { get_schemas } from './persistence'
 import { SethPhatToaster } from "./config/toaster";
 import { generateHashlink } from "./hashlink_generator";
 import { saveAs } from 'file-saver'
+import axios from 'axios';
 
 export const initOdcaCommunication = () => {
   eventBus.$on(EventHandlerConstant.ERROR, (error) => {
@@ -29,6 +30,36 @@ export const initOdcaCommunication = () => {
         Math.random().toString(16).substring(9)
 
       saveAs(blob, `${filename}.zip`);
+    }
+  })
+
+  eventBus.$on(EventHandlerConstant.PUBLISH_FORM, async ({ info, form }) => {
+    let schema
+
+    try {
+      const baseForm = get_schemas().find(s => s.uuid == form.uuid);
+      if (!baseForm) {
+          throw "Form cannot be exported"
+      }
+      schema = createSchemaFromForm(baseForm, form)
+    } catch(e) {
+      SethPhatToaster.error(e);
+    }
+
+    if (schema) {
+      const blob = await exportToZip(schema)
+
+      const fd = new FormData();
+      const file = new File([blob], "file.zip", { type: blob.type });
+      fd.append('file', file);
+
+      axios({
+        method: 'post',
+        url: `${info.host}/v2/schemas/${info.namespace}`,
+        data: fd
+      })
+        .then(r => window.open(r.data.url, '_blank'))
+        .catch(e => SethPhatToaster.error(e))
     }
   })
 
