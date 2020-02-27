027f246df5790398f733af7394f310d0ee004e20
Marcin <marcin@opensoftware.pl>
2020-02-27 15:17:56 +0100

    Handle GENERATE_HASHLINK event

--- src/hashlink_generator.js
+++ src/hashlink_generator.js
@@ -1,7 +1,6 @@
 const hl = require('hashlink')
 
-export const generateHashlink = async (data) => {
-    let urls, meta = undefined;
+export const generateHashlink = async (data, urls = undefined, meta = undefined) => {
     const codecs = ['mh-sha2-256', 'mb-base58-btc'];
     return await hl.encode({data, urls, codecs, meta});
 }
--- src/odcaCommunication.js
+++ src/odcaCommunication.js
@@ -76,4 +76,50 @@ export const initOdcaCommunication = () => {
     el.click()
     el.remove()
   })
+
+  eventBus.$on(EventHandlerConstant.GENERATE_HASHLINK, async ({ form, data, info }) => {
+    const dataStr = JSON.stringify(data, null, 2)
+    const buffer = Buffer.from(dataStr)
+    const hashlink = await generateHashlink(buffer)
+
+    let schema
+
+    try {
+      const baseForm = get_schemas().find(s => s.uuid == form.uuid);
+      if (!baseForm) {
+          throw "Form cannot be exported"
+      }
+      schema = createSchemaFromForm(baseForm, form)
+    } catch(e) {
+      SethPhatToaster.error(e);
+    }
+
+    if (schema) {
+      const { schemaBase, ...schemaOverlays } = schema
+      let schemaBaseId;
+      const overlayIds = []
+      for(let overlays of Object.values(schemaOverlays)) {
+          for(let [key, value] of Object.entries(overlays)) {
+            overlayIds.push(key.split(':')[1])
+            if(!schemaBaseId) {
+              schemaBaseId = value.schema_base.split(':')[1]
+            }
+          }
+      }
+
+      const urls = [`${info.fileserver}/${hashlink.split(':')[1]}.json`]
+      const meta = {
+        experimental: {
+          host: `${info.ocaRepo.host}/v2/schemas/${info.ocaRepo.namespace}/`,
+          'schema-base': schemaBaseId,
+          overlays: overlayIds.sort()
+        }
+      }
+
+      eventBus.$emit(EventHandlerConstant.HASHLINK_GENERATED, {
+        hashlink: await generateHashlink(buffer, urls, meta),
+        meta: { url: urls, ...meta }
+      })
+    }
+  })
 }
