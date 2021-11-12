857a98b5401ea11db9a9112afe4669a3f14752ac
Marcin Olichwiruk <marcin@opensoftware.pl>
2021-11-12 22:33:03 +0100

    Add filepicker for zipped OCA preview

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,34 +1,49 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <Form class="form" :structure="structures.test" default-language="en_EN" />
+  <q-page class="q-pa-md">
+    <div class="column items-center justify-evenly" style="height: 100vh">
+      <div class="col-1">
+        <q-file v-model="file" label="Pick OCA zip file" filled />
+      </div>
 
-    <Form
-      class="form"
-      readonly
-      :structure="structures.onboarding"
-      default-language="pl_PL" />
+      <div class="col-10">
+        <q-spinner v-if="loading" color="primary" size="5em" />
+        <Form
+          v-if="structure && !loading"
+          :key="structure"
+          class="form"
+          :structure="structure"
+          default-language="pl_PL" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
 import { components } from 'oca.js-form-quasar'
-import { createStructure } from 'oca.js-form-core'
-import type { Structure } from 'oca.js-form-core'
-import { ocaList } from '@/oca_list'
 const { Form } = components
+import { createStructure, resolveFromZip } from 'oca.js-form-core'
 
 export default defineComponent({
   name: 'Home',
   components: { Form },
   setup() {
-    const structures: {
-      [name: string]: Structure
-    } = {}
-    Object.entries(ocaList).forEach(([name, oca]) => {
-      structures[name] = createStructure(oca)
+    const structure = ref()
+    const file = ref()
+    const loading = ref(false)
+
+    watch(file, async newFile => {
+      loading.value = true
+      const oca = await resolveFromZip(newFile)
+      loading.value = false
+      structure.value = createStructure(oca)
     })
-    return { structures }
+
+    return {
+      structure,
+      loading,
+      file
+    }
   }
 })
 </script>
@@ -36,5 +51,7 @@ export default defineComponent({
 <style lang="scss">
 .form {
   width: 40vw;
+  min-width: 300px;
+  max-width: 600px;
 }
 </style>
